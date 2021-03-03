3359cdae58c4114e4758641ae5a57cd585dfe64f
Gabriel Zani <gabriel@eficienciafiscal.com.br>
2021-03-03 17:39:16 -0400

    Commit da quarta etapa do exercicio

--- ex_01/exercicio.php
+++ ex_01/exercicio.php
@@ -3,6 +3,7 @@
 $num_adv = random_int(0, 100);
 $pontuacao = 100;
 $resposta = "SIM";
+$hist_pont = [];
 
 do{
     $numero = (int) readline("Informe um número: ");
@@ -12,17 +13,24 @@ do{
     } else {
         if($numero > $num_adv){
             echo $num_adv." Você chutou muito alto. Tente um número menor!\n";          
-            $pontuacao -= 0.2;  
+            $pontuacao -= 0.2;              
         }
 
         if($numero < $num_adv){
             echo $num_adv." Você chutou muita baixo. Tente um número maior!\n";            
-            $pontuacao -= 0.2;
+            $pontuacao -= 0.2;            
         }
 
         if($numero == $num_adv){
+
+            $hist_pont[] = $pontuacao;
+
             echo"----------------------------------------------------------------\n";
-            echo "Parabéns você acertou :)\nSua pontuação final foi: ".$pontuacao."/100.\n"; 
+            echo "Parabéns você acertou :)\nSua pontuação final foi: ".$pontuacao."\n";
+
+            if($pontuacao > (int) max($hist_pont)){            
+                echo "NOVA PONTUAÇÃO!!!!!!!!! Sua pontuação agora é: ".$pontuacao."\n";
+            }
             echo"----------------------------------------------------------------\n";
 
             $resposta = strToUpper(readline("Deseja jogar novamente? SIM ou NÃO: ")); 
