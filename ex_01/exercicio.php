71ed556c7171756b3ad49a7bec6ee86aff89c01b
Gabriel Zani <gabriel@eficienciafiscal.com.br>
2021-03-03 15:56:00 -0400

    Commit da primeira etapa do exercicio

--- ex_01/exercicio.php
+++ ex_01/exercicio.php
@@ -1,4 +1,27 @@
 <?php
 
+$num_adv = random_int(0, 100);
+
+do{
+    $numero = (int) readline("Informe um número: ");
+
+    if(empty($numero)) {
+        echo "Por favor informe um número!\n";
+    } else {
+        if($numero > $num_adv){
+            echo "Você chutou muito alto. Tente um número menor!\n";            
+        }
+
+        if($numero < $num_adv){
+            echo "Você chutou muita baixo. Tente um número maior!\n";            
+        }
+
+        if($numero == $num_adv){
+            echo "Parabéns você acertou :)\n";
+            break;
+        }
+    }
+}
+while($numero != $num_adv);
 
 ?>
